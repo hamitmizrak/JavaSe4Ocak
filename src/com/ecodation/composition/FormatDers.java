3a8f118c96b601a3c1ab30d2099b6082fa1699b7
HamitMizrak <hamitmizrak@gmail.com>
2021-01-24 18:35:04 +0300

    underscore , ternary

--- src/com/ecodation/composition/FormatDers.java
+++ src/com/ecodation/composition/FormatDers.java
@@ -4,12 +4,25 @@ public class FormatDers {
 
 	public static void main(String[] args) {
 
-		// S D F
-		String kelime = "Hamit Mızrak";
-		int sayilar = 44;
-		float virgul = 19.44f;
+		String kelime = "";
+		final int sayilar = 0;
+		float virgul = 0;
+		try {
+			int telefonNumarasi = 555_44_33_22;
+			System.out.println(telefonNumarasi);
+			String ternary = 3 > 1 ? "büyük" : "küçük";
+			System.out.println(ternary);
+			// S D F
+			kelime = "Hamit Mızrak";
+			// sayilar = 44;
+			virgul = 19.44f;
+			int x = 4 / 0;
 
-		System.out.printf("%s %d %f ", kelime, sayilar, virgul);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.printf("%s %d %f ", kelime, sayilar, virgul);
+		}
 
 	}
 
